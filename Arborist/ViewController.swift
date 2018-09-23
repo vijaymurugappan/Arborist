4f148838c11a9724bcfe54194abd485cdf263f48
vijayms <vijaymurrugappan@gmail.com>
2018-09-23 11:37:49 -0700

    Updated commit
    Updated Create ML model for plant disease classifier and icons

--- Arborist/ViewController.swift
+++ Arborist/ViewController.swift
@@ -38,26 +38,24 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         observationView.frame = CGRect(x: 0, y: 540, width: view.frame.width, height: 128)
         setLabel(label: resultLabel, x: 30, y: 20, width: 80, height: 30, text: "Object :")
         setLabel(label: confidenceLabel, x: 30, y: 60, width: 120, height: 30, text: "Confidence :")
-        setButton(button: cureButton, x: 270, y: 60, width: 50, height: 30, text: "CURE ME")
+        setButton(button: cureButton, x: 270, y: 60, width: 90, height: 30, text: "CURE ME")
         view.addSubview(observationView)
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         captureSession.addOutput(dataOutput)
     }
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        self.objectLabel.text = ""
-        self.confidenceLevelLabel.text = ""
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
-        guard let model = try? VNCoreMLModel(for: Resnet50().model) else {return}
+        guard let model = try? VNCoreMLModel(for: PlantDiseaseClassifier().model) else {return}
         let vnRequest = VNCoreMLRequest(model: model) { (finishedReq, err) in
             guard let results = finishedReq.results as? [VNClassificationObservation] else {return}
             guard let recentObservation = results.first else {return}
             let confidence = (recentObservation.confidence * 100).rounded(.awayFromZero)
             if(confidence > 80) {
                 DispatchQueue.main.async {
-                    self.setLabel(label: self.objectLabel, x: 120, y: 20, width: self.view.frame.width - 10, height: 30, text: recentObservation.identifier)
-                    self.setLabel(label: self.confidenceLevelLabel, x: 160, y: 60, width: 100, height: 30, text: "\(confidence)%")
                     self.objectName = recentObservation.identifier
+                    self.setLabel(label: self.objectLabel, x: 120, y: 20, width: self.view.frame.width - 10, height: 30, text: self.objectName)
+                    self.setLabel(label: self.confidenceLevelLabel, x: 160, y: 60, width: 100, height: 30, text: "\(confidence)%")
                 }
             }
         }
@@ -74,8 +72,8 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     
     func setButton(button: UIButton, x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, text: String) {
         button.frame = CGRect(x: x, y: y, width: width, height: height)
-        button.titleLabel?.text = text
-        button.titleLabel?.textColor = UIColor.white
+        button.setTitle(text, for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         button.backgroundColor = UIColor.green
         button.addTarget(self, action: #selector(showCure), for: .touchUpInside)
